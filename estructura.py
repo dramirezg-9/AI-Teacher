83ca0c09046c36a3502e44966b7010d1e17872bc
netandout <netiel136@hotmail.com>
2020-04-16 21:03:09 -0500

    estructura.py

--- estructura.py
+++ estructura.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 12:03:43 2020
+
+@author: Netie
+"""
+
+
+from sympy import *
+
 def libre1(expresion:str)-> list:
     from parsing import parsing
     
@@ -36,4 +46,34 @@ def libre1(expresion:str)-> list:
             operador = expresion[i]
         i +=1
         
-    return libres 
+    return libres
+
+def evaluar_numerica(ecuacuacion:str,variables:dict)->int:
+    
+    for variable in variables:
+        ecuacuacion = ecuacuacion.replace(variable, str(variables[variable]))
+    resultado = eval(ecuacuacion)
+    return resultado
+
+
+def separar_igualdad(ecuacion:str)->list:
+    partes = [-2]
+    comp = []
+    ecuacion =   ecuacion
+    for i in range(len(ecuacion)):
+        if ecuacion[i] in "=<>":
+             partes.append(i-1)
+    partes.append(len(ecuacion)-1)
+    for j in range(len(partes)-1):
+        print(partes[j])
+        print(partes[j+1])
+        comp.append(ecuacion[partes[j]+2:partes[j+1]+1])
+    return comp
+
+
+
+    
+
+    
+    
+    
