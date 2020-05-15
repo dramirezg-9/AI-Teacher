a89e92ddbf5bbd32c9847e6e25da9003c4ad53a0
orion160 <isaacdavid719@gmail.com>
2020-05-14 20:07:05 -0500

    Added some integrity to views and templates

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('features/', views.features, name='features'),
 ]
